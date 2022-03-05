5e19952c9fa8a118127ccd6f2e1f3b9f919d9d5b
Khoi Lam <54829621+kal038@users.noreply.github.com>
2022-03-05 17:19:08 -0500

    Time: 15 ms (94.65%), Space: 13.6 MB (31.52%) - LeetHub

---
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        '''
+        Don't need to define an inner function because we don't have to pass extra info into it.
+        Recurisve solution
+        
+        '''
+        
+        if not head or not head.next:
+            return head
+        temp = head.next.next
+        ret = head.next
+        head.next.next = head;
+        head.next = self.swapPairs(temp)
+        return ret
