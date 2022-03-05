71e3d4b092583de6dcb3041261711dab8b459506
Khoi Lam <54829621+kal038@users.noreply.github.com>
2022-03-05 16:40:42 -0500

    Time: 150 ms (15.03%), Space: 15.1 MB (14.67%) - LeetHub

---
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.py
@@ -0,0 +1,44 @@
+import collections
+class Solution(object):
+    def allPathsSourceTarget(self, graph):
+        """
+        :type graph: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        '''
+        backtracking algorithm
+        
+        allPathsSourceTarget(graph)
+        
+        result = []
+        target = last numbered node
+        
+        backtrack(node, curr_path):
+            base case if node == target:
+                append curr_path to result
+            loop through the array of neighbors
+                add the current choice to path
+                explore recursively
+                remove the choice from path
+                
+        backtrack(0, [0])
+        return results
+                
+        '''
+        
+        target = len(graph) - 1
+        results = []
+        
+        def backtrack(node, curr_path):
+            if node == target:
+                results.append(list(curr_path))
+                return
+            for neigh in graph[node]:
+                curr_path.append(neigh)
+                backtrack(neigh, curr_path)
+                curr_path.pop()
+                
+        path  = collections.deque([0])
+        backtrack(0, path)
+        
+        return results
