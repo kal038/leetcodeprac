82bfc51f47f9034c228f0647950a7c5fcb1dc068
Khoi Lam <54829621+kal038@users.noreply.github.com>
2022-03-08 11:10:08 -0500

    Time: 121 ms (51.96%), Space: 16.8 MB (55.21%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -8,7 +8,18 @@ class Solution(object):
         """
         '''
         top k frequent is most likely to use a heap here with O(Nlogk) time and O(N+k) space for the hm and the heap
+        
+        Pseudocode:
+            create a counter dictionary to count the frequency of the values in nums
+            insert those values in a heap and extract the k largest values with the return values as they keys of the Counter and the sorting criteria as the value of the Counter using count.get (which return a comparable object (the frequency))
+            
+        Syntax:
+        heapq.nlargest(k, [things to return], [sotring criteria in terms of a function called with no ()])
         '''
         count = collections.Counter(nums)
         
-        return heapq.nlargest(k, count.keys(), key = count.get)
+        print(count.get(1))
+        print(count.get(2))
+        print(count.get(3))
+        
+        return heapq.nlargest(k, count.keys(), key = count.get) #key = [3,2,1]
