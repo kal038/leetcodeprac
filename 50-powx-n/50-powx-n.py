e19398402aab37d77f08abb2864f51c7c27ff292
Khoi Lam <54829621+kal038@users.noreply.github.com>
2022-03-08 16:44:52 -0500

    Time: 29 ms (36.98%), Space: 13.2 MB (90.82%) - LeetHub

--- 50-powx-n/50-powx-n.py
+++ 50-powx-n/50-powx-n.py
@@ -1,20 +1,33 @@
 class Solution(object):
     def myPow(self, x, n):
-        """
-        :type x: float
-        :type n: int
-        :rtype: float
-        """
-        t=abs(n)
-        ans=1
-        while(t>0):
-            if(t%2==0):
-                x=x*x
-                t=t//2
-            else:
-                ans=ans*x
-                t=t-1
-        if(n<0):
-            return 1/ans
-        else:
-            return ans
+        '''
+        O(n) solution just don't work
+        O(logn) solution: Divide and conquer
+        
+        '''
+        
+        def helper(x, n):
+            if x == 0: return 0
+            if n == 0: return 1
+            
+            res = helper(x, n//2)
+            res = res * res
+            return (x*res) if n % 2 else res
+        
+        res = helper(x, abs(n))
+        return res if n >= 0 else 1/res
+        # t=abs(n)
+        # ans=1
+        # while(t>0):
+        #     if(t%2==0):
+        #         x=x*x
+        #         t=t//2
+        #     else:
+        #         ans=ans*x
+        #         t=t-1
+        # if(n<0):
+        #     return 1/ans
+        # else:
+        #     return ans
+        
+     
