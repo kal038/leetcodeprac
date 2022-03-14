c05559f2627caf276567b7e92af64d8a71e688da
Khoi Lam <54829621+kal038@users.noreply.github.com>
2022-03-14 09:52:14 -0400

    Time: 25 ms (56.89%), Space: 13.5 MB (36.92%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.py
+++ 191-number-of-1-bits/191-number-of-1-bits.py
@@ -4,13 +4,13 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        ans = 0 
-        while n>0:
-        # it just checks if right bit is set or not 
-            if n&1: 
-                ans +=1 
+#         ans = 0 
+#         while n>0:
+#         # it just checks if right bit is set or not 
+#             if n&1: 
+#                 ans +=1 
 
-            # removes the last bit 
-            n>>=1    
+#             # removes the last bit 
+#             n>>=1    
 
-        return ans
+        return bin(n).count("1")
