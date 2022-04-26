dc43f8160484302c65cadd732721948494a720f0
Khoi Lam <54829621+kal038@users.noreply.github.com>
2022-04-26 18:12:00 -0400

    Time: 107 ms (55.31%), Space: 14.9 MB (47.30%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.py
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.py
@@ -1,5 +1,15 @@
 class Solution:
     def removeDuplicates(self, s: str) -> str:
+        '''
+        Use a stack to keep track and to produce the final results of the problem
+        For example: "abbaca"
+        Start the stack with stack = [a]
+        iterate through starting from second index:
+        see if the current letter is on top of stack using peek(), if it is, pop from stack
+        if it is not, append the current letter to the stack
+        This will leave a stack that has elements that represent the resulting string in the end and all we need
+        to do is "".join
+        '''
         stack = []
         stack.append(s[0])
         for i in range(1,len(s)):
