edeafc46a76f89dc7f28d1b054b8ea67a2d8894d
Khoi Lam <54829621+kal038@users.noreply.github.com>
2022-04-27 07:42:06 -0400

    Time: 100 ms (45.84%), Space: 29.7 MB (7.56%) - LeetHub

---
+++ 740-delete-and-earn/740-delete-and-earn.py
@@ -0,0 +1,25 @@
+class Solution:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        '''
+        topdown dynamic programming solution using @lru_cache to memoize the recursive call
+        '''
+        
+        points = defaultdict(int)
+        max_number = 0
+        # Precompute how many points we gain from taking an element
+        for num in nums:
+            points[num] += num
+            max_number = max(max_number, num)
+            
+        print(points)
+        print(max_number)
+        @lru_cache
+        def max_points(num):
+            if num == 0:
+                return 0
+            if num == 1:
+                return points[1]
+            
+            return max(max_points(num - 1), max_points(num-2) + points[num])
+        
+        return max_points(max_number)
