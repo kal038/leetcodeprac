2f9b28b6b825299840842223400c343d073063f3
Khoi Lam <54829621+kal038@users.noreply.github.com>
2022-04-27 08:01:50 -0400

    Time: 88 ms (60.59%), Space: 16.5 MB (22.98%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.py
+++ 740-delete-and-earn/740-delete-and-earn.py
@@ -1,22 +1,25 @@
+from collections import Counter
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
         '''
-        topdown dynamic programming solution using @lru_cache to memoize the recursive call
+        bottom up solution, build the array that, at each index, stores the max earning of all values considered up to that point
+        for example: [2,3,5,7,8]
+        dp array:    [2,3,8,...]in which 8 is the max amount we could earn when considering the subarray [2,3,5]
+                        e1,e2
+        further optimization, we recognize that we only need to store the value 1 and 2 index in front of the element considered, reduce memory by just using 2pointers and update them e1, e2
         '''
-        
-        points = defaultdict(int)
-     
-        # Precompute how many points we gain from taking an element
-        for num in nums:
-            points[num] += num
-        
-        @lru_cache
-        def max_points(num):
-            if num == 0:
-                return 0
-            if num == 1:
-                return points[1]
-            
-            return max(max_points(num - 1), max_points(num-2) + points[num]) #either don't take the current value and take the previous' or take the current and the one 2 units away
-        
-        return max_points(max(nums))
+        count = Counter(nums)
+        nums = sorted(list(set(nums)))
+        e1, e2 = 0, 0
+        for i in range(len(nums)):
+            curr_e = nums[i] * count[nums[i]] # value * frequency = total earned, from here divide into two cases. 1: the value right before is not a violation -> awesome. 2: the value right before IS a violation -> recurrence relation
+            if i > 0 and nums[i] == nums[i-1] + 1: 
+                temp = e2
+                e2 = max(curr_e + e1, e2)
+                e1 = temp
+            else:
+                temp = e2
+                e2 = curr_e + temp 
+                e1 = temp #increment e1 to where e2 once was
+                
+        return e2
